2a90b3927be2944cec207fec84750562bbe99afa
mikeen97 <mike97@hotmail.es>
2017-07-28 12:03:08 -0600

    Priera versa

---
+++ PRueba/src/prueba/PRueba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author Miguel Flores
+ */
+public class PRueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Te la comes toda");
+    }
+    
+}
